857f88fd5e2b2b2b20436514736c0470baba774f
hoangpersie <hoangpersievn@gmail.com>
2019-12-13 13:55:53 +0700

    Updates

---
+++ precache-manifest.00a3522194892fed8b45e2be1e6e12c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa519659047b2672b47f81163416778",
+    "url": "/hoangpersievn/chat-io-client/index.html"
+  },
+  {
+    "revision": "49d0ae8c178143f719eb",
+    "url": "/hoangpersievn/chat-io-client/static/css/main.9e0893f5.chunk.css"
+  },
+  {
+    "revision": "1670fd816177f7ca300e",
+    "url": "/hoangpersievn/chat-io-client/static/js/2.78cc99f9.chunk.js"
+  },
+  {
+    "revision": "c1164b431e4b2768c27fd9959213b22d",
+    "url": "/hoangpersievn/chat-io-client/static/js/2.78cc99f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49d0ae8c178143f719eb",
+    "url": "/hoangpersievn/chat-io-client/static/js/main.5bfb2cfe.chunk.js"
+  },
+  {
+    "revision": "08ffa405a05ea68294a6",
+    "url": "/hoangpersievn/chat-io-client/static/js/runtime-main.421e6542.js"
+  }
+]);
